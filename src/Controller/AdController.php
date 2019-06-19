1e4b0e3a33746b25e7b72d0fb7f7eb1e8a66e0b6
b0ndurant <martin.aurelien01@gmail.com>
2019-06-20 01:46:12 +0200

    add function index and show in controller AdController and create structur html ad/index.html

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,18 +2,37 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class AdController extends AbstractController
+class AdController extends Controller
 {
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index()
+    public function index(AdRepository $repo)
     {
+        $ads = $repo->findAll();
+
         return $this->render('ad/index.html.twig', [
-            'controller_name' => 'AdController',
+            'ads' => $ads,
+        ]);
+    }
+
+    /**
+     * show ad
+     * 
+     * @Route("/ads/{slug}", name="ads_show")
+     * 
+     * @return Response
+     */
+    public function show(adRepository $repo, $slug) 
+    {
+        $ad = $repo->findOneBySlug($slug);
+
+        return $this->render('ad/show.html.twig', [
+            "ad" => $ad,
         ]);
     }
 }
