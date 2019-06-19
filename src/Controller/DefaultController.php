6976fead02c868a22a8cdb1502d24248debb0bf2
b0ndurant <martin.aurelien01@gmail.com>
2019-06-19 19:38:04 +0200

    create structure sf4 and add homepage with controller and entity Ad + Image

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController {
+    
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(){
+        return $this->render(
+            'homepage.html.twig'
+        );
+
+    }
+}
+
+?>
